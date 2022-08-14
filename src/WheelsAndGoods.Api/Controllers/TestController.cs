1954359cc3324b911aa393e0a8062b5c5f978b4a
Alexandr Kirpichyov <alexandr.kirpichyov@gmail.com>
2022-08-14 17:57:07 +0300

    Add temp tests controller

---
+++ src/WheelsAndGoods.Api/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WheelsAndGoods.Api.Controllers;
+
+// NOTE: Will be removed after configs testing completion.
+[AllowAnonymous]
+public class TestController : ApiControllerBase
+{
+	private readonly IConfiguration _configuration;
+
+	public TestController(IConfiguration configuration)
+	{
+		_configuration = configuration;
+	}
+
+	[HttpGet]
+	public string GetConfig()
+	{
+		return _configuration["TestConfig"];
+	}
+}
